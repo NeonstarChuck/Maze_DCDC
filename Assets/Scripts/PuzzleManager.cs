14a0b9109fdfb37a97d9bdfba99d2e23f38503f4
Chuck Long Ching <131591076+NeonstarChuck@users.noreply.github.com>
2026-05-12 20:05:02 +0200

    Room generation fix

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -2,33 +2,40 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    public Door1 door;
+    public Door1 stage1Door;
+    public Door1 stage2Door;
 
-    private string[] sequence = new string[]
+    private readonly string[][] sequences =
     {
-        "Red",
-        "Yellow",
-        "Green",
-        "Blue",
-       
+        new string[] { "Red", "Yellow", "Green", "Blue" },
+        new string[] { "Blue", "Green", "Yellow", "Red" }
     };
 
+    private int currentStage = 0;
     private int index = 0;
 
     public void PressButton(string color)
     {
+        if (currentStage >= sequences.Length)
+        {
+            Debug.Log("All stages completed!");
+            return;
+        }
+
         Debug.Log("Pressed: " + color);
 
-        // correct step
-        if (color == sequence[index])
+        if (color == sequences[currentStage][index])
         {
             index++;
-            Debug.Log("Correct step: " + index + "/" + sequence.Length);
+            Debug.Log("Correct step: " + index + "/" + sequences[currentStage].Length);
 
-            if (index >= sequence.Length)
+            if (index >= sequences[currentStage].Length)
             {
-                Debug.Log("PUZZLE SOLVED!");
-                door.Open();
+                Debug.Log("STAGE " + (currentStage + 1) + " SOLVED!");
+
+                OpenCurrentDoor();
+
+                currentStage++;
                 ResetPuzzle();
             }
         }
@@ -39,7 +46,19 @@ public class PuzzleManager : MonoBehaviour
         }
     }
 
-    void ResetPuzzle()
+    private void OpenCurrentDoor()
+    {
+        if (currentStage == 0 && stage1Door != null)
+        {
+            stage1Door.Open();
+        }
+        else if (currentStage == 1 && stage2Door != null)
+        {
+            stage2Door.Open();
+        }
+    }
+
+    private void ResetPuzzle()
     {
         index = 0;
     }
